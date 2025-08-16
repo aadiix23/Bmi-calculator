747d1250fa1ee01ef4e91747da88451ba2a2f604
aadiix23 <aadiix23@gmail.com>
2025-08-16 16:15:01 +0530

    added weight description

--- script.js
+++ script.js
@@ -14,4 +14,22 @@ form.addEventListener('submit' ,function (e){
     const bmi = (weight/((height*height)/10000)).toFixed(2);
     results.innerHTML=`<span>${bmi}</span>`;
   }
+
 })
+const formm= document.querySelector('form');
+formm.addEventListener('submit' ,function (f){
+  f.preventDefault();
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const define=document.querySelector('#bmi-weight')
+  const bmi = (weight/((height*height)/10000)).toFixed(2);
+  if(bmi<18.6){
+    define.innerHTML=`<span>Underweight</span>`
+   }
+  else if(bmi>24.9){
+   define.innerHTML=`<span>overweight</span>`
+  }
+  else if(bmi>18.6 && bmi<24.9) {
+    define.innerHTML=`<span>Normal</span>`
+  }
+});
